6910e87f91fb3a4d89bcd244b85b5a5ae72fe285
olsax <lolzzcake@gmail.com>
2020-09-13 00:36:04 +0200

    clean needed data

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ Showing statistics about the number of girls attending technical universities in
 
 The data was taken from [Domain Knowledge Bases](http://swaid.stat.gov.pl/en/Edukacja_dashboards/Raporty_predefiniowane/RAP_DBD_EDU_12.aspx)
 
-1. read&clean the data
+Current list of steps:
+1. Read&clean the data
 2. 
+
+### Language
+Python 3.x
+
+### Cloning
+- Clone this repo to your local machine using https://github.com/olsax/girls-in-unis.git
+
+### Running the code
+
+```
+py run.py
+```
 
--- run.py
+++ run.py
@@ -1,36 +1,33 @@
 import pandas as pd
 import csv
 
-
-#read_file_pd = pd.read_csv('girls_in_tech_unis.csv', header=None)
-#print(read_file_pd)
-
 largest_column_cnt = 0
 
 with open('girls_in_tech_unis.csv', 'r') as file_csv:
 	read_file = csv.reader(file_csv)
-	#for row in read_file:
-	#	print(row)
 	lines = file_csv.readlines()
 	for l in lines:
 		column_cnt = len(l.split(',')) + 1
 		largest_column_cnt = column_cnt if largest_column_cnt < column_cnt else largest_column_cnt
-
 file_csv.close()
 
 column_names = [i for i in range(0, largest_column_cnt)]
 
 df = pd.read_csv('girls_in_tech_unis.csv', header=None, delimiter = ',', names=column_names)
-df = df.drop([0, 2, 3, 4])
-df = df.drop(columns = [0])
-df = df.reset_index(drop=True)
-df = df.dropna(axis=1, how='all')
+df = df.drop([0, 2, 3, 4]).drop(columns = [0])
+df = df.reset_index(drop=True).dropna(axis=1, how='all')
 
 data_info = df.iloc[0][1]
 
 df = df.drop([0])
-df = df.reset_index(drop=True)
-print(df)
+df = df.reset_index(drop=True).rename(columns={1: "Area", 2: "Year", 3: "Count"})
 
 data_info = data_info.splitlines()
-print(data_info)
+
+years_str = data_info[0][6:]
+years_int = years_str.split(", ")
+years_int = [int(year) for year in years_int]
+
+areas = data_info[1][18:]
+areas = areas.split(", ")
+
