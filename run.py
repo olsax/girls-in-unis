367588f7f3d4ad9cff697cf3255e15183bc8682c
olsax <lolzzcake@gmail.com>
2020-10-03 14:33:33 +0200

    add chart 1

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+# libraries and data
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import numpy as np
 import pandas as pd
 import csv
 
@@ -17,17 +22,28 @@ df = pd.read_csv('girls_in_tech_unis.csv', header=None, delimiter = ',', names=c
 df = df.drop([0, 2, 3, 4]).drop(columns = [0])
 df = df.reset_index(drop=True).dropna(axis=1, how='all')
 
-data_info = df.iloc[0][1]
-
 df = df.drop([0])
-df = df.reset_index(drop=True).rename(columns={1: "Area", 2: "Year", 3: "Count"})
+df = df.reset_index(drop=True).rename(columns={1: "area", 2: "year", 3: "count"})
+df['count'] = pd.to_numeric(df['count'])
+df['count'] = df['count'].fillna(0)
 
-data_info = data_info.splitlines()
+#dataframe without POLSKA values
+df_wp = df[df.area != 'POLSKA']
 
-years_str = data_info[0][6:]
-years_int = years_str.split(", ")
-years_int = [int(year) for year in years_int]
+#plot work
+mpl.style.use('seaborn')
+sns.set_style("darkgrid")
+sns.set_palette("deep", 14)
+fig, ax = plt.subplots()
 
-areas = data_info[1][18:]
-areas = areas.split(", ")
+for key, grp in df_wp.groupby(['area']):
+    ax = grp.plot(ax=ax, kind='line', x='year', y='count', label=key, figsize=(9,4))
+
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+plt.legend(loc='upper center', bbox_to_anchor=(1.2, 1.0),
+          ncol=1, fancybox=True, shadow=True, fontsize='small')
+plt.title("number of girls attending technical universities in 2010 to 2015 in each of the voivodeships in Poland", fontsize=11, backgroundcolor='lavender', color='slategrey', fontweight='bold', pad=10)
+
+plt.show()
 
